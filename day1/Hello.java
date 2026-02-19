646bdbf60ffb87cdd0f526283d174065f9de7425
M <rathiess@amazon.com>
2026-02-19 21:29:05 +0530

    completed hello world

--- day1/Hello.java
+++ day1/Hello.java
@@ -2,6 +2,6 @@ package day1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Day 1 of the challenge!");
+        System.out.print("Hello World");
     }
 }
