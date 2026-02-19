4b2ee516053e0944559fc7500a7f114694412fb9
M <rathiess@amazon.com>
2026-02-19 21:13:22 +0530

    completed datatypes and variables

---
+++ day3/demo.java
@@ -0,0 +1,15 @@
+ 
+ package day3;
+
+public class demo {
+
+    public static void main(String[] args) {
+         
+        int container = 10;
+        int container2 = 20;
+
+        System.out.print(container+container2);
+
+    }
+
+}
