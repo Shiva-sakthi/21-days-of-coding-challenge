27d78b887a8ed88edebd084a7df0544831619f92
M <rathiess@amazon.com>
2026-02-19 21:25:36 +0530

    completed loop

--- day2/loop.java
+++ day2/loop.java
@@ -1,4 +1,4 @@
- public class 
+  class loop{
     public static void main(String[] args) {
         // Loop starts at 1, runs as long as i <= 5, and adds 1 to i each time
         for (int i = 1; i <= 5; i++) {
