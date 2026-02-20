b6d2473092b6790d1ae390119d8924a902bbd6a5
Shiva-sakthi <sivasmith377@gmail.com>
2026-02-20 23:09:46 +0530

    get user input.

---
+++ day4/user.java
@@ -0,0 +1,17 @@
+package day4;
+
+import java.util.Scanner ;
+
+public class user {
+    
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int temp = scan.nextInt();
+        System.out.print(temp);
+
+
+
+    }
+}
