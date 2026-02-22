1c66400b666bb1a0593e2bb58e451263c08e6d2d
Shiva-sakthi <sivasmith377@gmail.com>
2026-02-22 18:39:58 +0530

    practice some problems

--- day5/recall.java
+++ day5/recall.java
@@ -1,5 +1,22 @@
+
 package day5;
 
+import java.util.Scanner ;
+
 public class recall {
-    
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String name= scan.nextLine();
+        int age = scan.nextInt();
+        scan.nextLine();
+        String address = scan.nextLine();
+
+        System.out.println("my name is "+name);
+        System.out.println("my age is "+age);
+        System.out.println("my address is "+address);
+
+    }
 }
