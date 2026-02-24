7ed178f8a9e45d6c47cf501caa8240148f3ddd96
Shiva-sakthi <sivasmith377@gmail.com>
2026-02-24 22:12:55 +0530

    if else

--- day7/ifelse.java
+++ day7/ifelse.java
@@ -2,6 +2,14 @@ package day7;
 
 public class ifelse {
     public static void main(String[] args) {
-        
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote!");
+        } else {
+            System.out.println("You are too young to vote.");
+        }
     }
-}
+
+    }
+
