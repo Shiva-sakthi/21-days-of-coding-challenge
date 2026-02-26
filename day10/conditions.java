84196702bc1382be876e3b0fa7df673cafaacda6
Shiva-sakthi <sivasmith377@gmail.com>
2026-02-26 22:04:45 +0530

    else if and nested if

---
+++ day10/conditions.java
@@ -0,0 +1,39 @@
+package day10;
+
+public class conditions {
+    public static void main(String[] args) {
+        
+        //int score = 45;
+
+        //if (score>30) {
+          //  System.out.print("keep going");
+     //       
+       // }
+    //    else if (score>60) {
+
+      //      System.out.print("goood");
+
+
+            
+     //   }
+      //  else if (score>90){
+        //    System.out.print("very goood");
+       // }
+
+       boolean mall=true;
+       boolean theatre = false;
+
+       if (mall) {
+        System.out.print("buy the snacks and book the ticket");
+
+        if (theatre) {
+
+            System.out.print("watch the movie");
+            
+        }
+        
+       }
+
+    }
+}
+
