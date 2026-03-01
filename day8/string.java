c630bba650d97bb6ebcb6849a7ae122749086cc2
Shiva-sakthi <sivasmith377@gmail.com>
2026-03-01 17:11:35 +0530

    string comparator

--- day8/string.java
+++ day8/string.java
@@ -1,7 +1,30 @@
 package day8;
-
+import java.util.Scanner;
 public class string {
     public static void main(String[] args) {
-        System.out.print("hello");
+        //String a = "one";
+        //String b = new String("one");
+        //String c=b;
+
+        //System.out.print(a==c);
+
+        Scanner scan = new Scanner(System.in);
+       
+
+        System.out.println("enter the rcb victory or not");
+        String RCB = scan.nextLine();
+
+        if (RCB.equals("win")){
+            System.out.println("Saala cup namthe");
+
+        }
+        else{
+            System.out.println("cup illa");
+        }
+
+
+        
+        
+
     }
 }
