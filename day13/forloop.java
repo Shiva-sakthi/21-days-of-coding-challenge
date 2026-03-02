5762188f8995c693f420c4521278678cc3544fac
Shiva-sakthi <sivasmith377@gmail.com>
2026-03-02 22:45:50 +0530

    for loop

---
+++ day13/forloop.java
@@ -0,0 +1,13 @@
+package day13;
+
+public class forloop {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=10;i++){
+
+            System.out.println(i);
+
+        }
+    }
+
+}
