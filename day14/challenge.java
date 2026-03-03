198b890cc3d229e3b784d9f2e66b1218b8a29e19
Shiva-sakthi <sivasmith377@gmail.com>
2026-03-03 20:28:00 +0530

    practice problems based for loop

---
+++ day14/challenge.java
@@ -0,0 +1,49 @@
+package day14;
+import java.util.Scanner;
+
+
+public class challenge {
+    public static void main (String[] args){
+
+       // Scanner scan = new Scanner(System.in);
+
+        //System.out.print("enter the num1:");
+        //int a = scan.nextInt();
+
+//System.out.print("enter the num2:");
+//int b = scan.nextInt();
+
+//for(int i =a; i<=b;i++){
+
+  //  System.out.println(i);
+//}
+
+
+//Scanner scan = new Scanner(System.in);
+//System.out.print("enter the num1: ");
+//int a = scan.nextInt();
+//System.out.print("enter the num2: ");
+//int b = scan.nextInt();
+
+//int evencount=0;
+//for(int i =a; i<=b;i++){
+
+  //  if (i%2==0) {
+    //    evencount=evencount+1;
+   // }
+//}
+  //System.out.println(evencount);
+
+// this is okk but i count only means this code is okk!
+
+//print the number from 1 to 100 . the number is divisible by both 3 and 5 . 
+    
+    for(int i=1; i<=100;i++){
+        if (i%3==0 && i%5==0) {
+            System.out.println(i);
+        }
+    } 
+    
+}
+
+}
