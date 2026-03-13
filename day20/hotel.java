5aab6bfd663a5f5ce88e0badcc8796d0f313ff23
Shiva-sakthi <sivasmith377@gmail.com>
2026-03-13 21:10:32 +0530

    classes and objects

---
+++ day20/hotel.java
@@ -0,0 +1,12 @@
+public class hotel {
+    int dosa = 20;
+    int tea = 10;
+    int parotta = 20;
+
+    public static void main(String[] args) {
+        
+        hotel server1 = new hotel();
+        System.out.print(server1.dosa);
+
+    }
+}
